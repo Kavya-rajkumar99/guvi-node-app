f61e6aea1ce36fa7f157e033602f16f6639c98b8
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-07-14 12:01:49 +0530

    commit changes

--- routes/helper.js
+++ routes/helper.js
@@ -1,16 +1,17 @@
 import { client } from '../index.js';
+import {ObjectId} from "mongodb";
 
 export async function updateMovieById(id, data) {
-    return await client.db("guvi-db").collection("movies").updateOne({ id: id }, { $set: data });
+    return await client.db("guvi-db").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-    return await client.db("guvi-db").collection("movies").deleteOne({ id: id });
+    return await client.db("guvi-db").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function createMovies(data) {
     return await client.db("guvi-db").collection("movies").insertMany(data);
 }
 export async function getMovieById(id) {
-    return await client.db("guvi-db").collection("movies").findOne({ id: id });
+    return await client.db("guvi-db").collection("movies").findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies(request) {
     return await client.db("guvi-db").collection("movies").find(request.query).toArray();
